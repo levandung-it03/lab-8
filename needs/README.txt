b6a860b46e547a19c6dc642387976259268c44a7
levandungit03 <levandungit03@gmail.com>
2023-12-12 19:53:34 +0700

    Fix Bug

--- needs/README.txt
+++ needs/README.txt
@@ -14,20 +14,3 @@
 CREATE DATABASE Student_Management
 
 -- 5. Open new SQL Query:
-USE Student_Management;
-CREATE TABLE Student (
-    student_id VARCHAR(20) UNIQUE NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-	first_name VARCHAR(50) NOT NULL,
-	grade_code VARCHAR(20) NOT NULL,
-	grade_name VARCHAR(100) NOT NULL,
-	phone VARCHAR(20) NOT NULL,
-	email VARCHAR(30) UNIQUE NOT NULL,
-    CONSTRAINT PK_Student PRIMARY KEY (student_id),
-    CONSTRAINT UK_student_id_grade_code UNIQUE (student_id, grade_code)
-);
-INSERT INTO Student (student_id, last_name, first_name, grade_code, grade_name, phone, email)
-VALUES
-	('N21DCCN021', 'Le Van', 'Dung', 'D21CQCN01-N', 'Cong Nghe Thong Tin 01', '0377863928', 'levandung.it03@gmail.com'),
-	('N21DCCN001', 'Nguyen Van', 'A', 'D21CQCN01-N', 'Cong Nghe Thong Tin 01', '0377863930', 'nguyenvana.2003@gmail.com'),
-	('N21DCCN002', 'Le Van', 'B', 'D21CQCN01-N', 'Cong Nghe Thong Tin 01', '0377863950', 'levanb.2003@gmail.com');
--- src/com/StudentManagement/DBConnection/DBInteraction.java
+++ src/com/StudentManagement/DBConnection/DBInteraction.java
@@ -3,28 +3,34 @@ package com.StudentManagement.DBConnection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.SQLSyntaxErrorException;
 
 public class DBInteraction {
     public DBInteraction() { super(); }
 
     public static Connection startDBConnection() {
         Connection myConnection = null;
-        final String dbURL = "jdbc:mysql://localhost:3306/Student_Management";
+
+        final String dbURL = "jdbc:mysql://localhost:3306/";
+        final String dbName = "Student_Management";
         final String username = "root";
         final String password = "";
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            myConnection = DriverManager.getConnection(dbURL, username, password);
+            myConnection = DriverManager.getConnection(dbURL + dbName, username, password);
+
+        } catch (SQLSyntaxErrorException ignored) {
+            myConnection = DBInitialization.initializeDB(dbURL, dbName, username, password);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
 
-        if (myConnection != null) {
+        if (myConnection != null)
             System.out.println("Connect to DB Successfully!");
-        } else {
+        else
             System.out.println("Connect to DB Failed!");
-        }
+
         return myConnection;
     }
 
